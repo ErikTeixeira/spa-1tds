15b01adc8bdc2ee21f75da17c64f69cd8f934fe2
LeooGSantos <rm98912@fiap.com.br>
2023-10-10 21:45:31 -0300

    Alteração na main

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,6 @@ const router = createBrowserRouter(
 
 
 //BLOCO DE ROTAS
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
