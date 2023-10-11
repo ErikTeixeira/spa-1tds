612b1afad8165f2d5fc2e63d2275723b45d83102
AlleSilvaa <allessonaugusto10@gmail.com>
2023-10-10 21:10:14 -0300

    editar e excluir atualizado e add

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,71 +1,87 @@
-import { useState } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
-import { ListaProdutos } from '../components/ListaProdutos';
- 
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import "./EditarProdutos.scss"; 
+
 export default function EditarProdutos() {
   document.title = "EDITAR PRODUTO";
 
   const navigate = useNavigate();
   const { id } = useParams();
-  const produtoRecuperado = ListaProdutos.filter((produto) => produto.id == id)[0]
   const [produto, setProduto] = useState({
-    id: produtoRecuperado.id,
-    nome: produtoRecuperado.nome,
-    desc: produtoRecuperado.desc,
-    img: produtoRecuperado.img,
-    preco: produtoRecuperado.preco,
-  })
+    id: id,
+    nome: '',
+    desc: '',
+    img: '',
+    preco: '',
+  });
+
+  useEffect(() => {
+    // Esta função é executada quando o componente é montado ou quando 'id' muda.
+    // Faz uma solicitação GET para obter os detalhes do produto da API.
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProduto(data); // Define os detalhes do produto recuperados da API no estado
+      })
+      .catch((error) => console.log(error));
+  }, [id]);
+
   
-  const handleChange = (e)=>{
-    //Destructuring
-    const {name,value} = e.target;
-    //Populando o state produto com as propriedades dos inputs que estão sendo ativados, utilizando SPREAD.
-    setProduto({...produto,[name]:value});
+  const handleChange = (e) => {
+    // Esta função é chamada quando os campos de entrada são alterados.
+    // Atualiza o estado 'produto' com os valores dos campos de entrada.
+    const { name, value } = e.target;
+    setProduto({ ...produto, [name]: value });
   }
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    let indice;
-    //Recuperando o índice do produto alterado com forEach.
-    // ListaProdutos.forEach((item,index)=>{
-    //   if(item === produto){
-    //     indice = index;
-    //   }
-    // });
-
-    //Ou utilizando o método indexOf
-    indice = ListaProdutos.findIndex(item => item.id == produto.id);
-
-    //Alterando o produto na lista com o método splice()
-    ListaProdutos.splice(indice,1,produto);
-
-    //Redirecionando o usuáio para a página de produtos!
-    navigate('/produtos');
+    
+    // Esta função é chamada quando o formulário é enviado.
+    // Faz uma solicitação PUT para atualizar o produto na API.
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(produto),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        navigate('/produtos'); // Redireciona para a página de produtos após a edição.
+      })
+      .catch((error) => console.log(error));
   }
+
+
   return (
-    <>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <fieldset>
-              <legend>Produto Selecionado</legend>
-              <div>
-                <label htmlFor="idNome">Nome</label>
-                <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
-              </div>
-              <div>
-                <label htmlFor="idDesc">Descrição</label>
-                <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange}/>
-              </div>
-              <div>
-                <label htmlFor="idPreco">Preço</label>
-                <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange}/>
-              </div>
-              <div>
-                <button>EDITAR</button>
-              </div>
-            </fieldset>
-          </form>
-        </div>
-    </>
+    <div className="EditarProdutos">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Editar Produto</legend>
+          <div>
+            <label htmlFor="idNome">Nome</label>
+            <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange} />
+          </div>
+          <div>
+            <label htmlFor="idDesc">Descrição</label>
+            <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange} />
+          </div>
+          <div>
+            <label htmlFor="idPreco">Preço</label>
+            <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange} />
+          </div>
+          <div>
+            <button type="submit">EDITAR</button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
   );
 }
